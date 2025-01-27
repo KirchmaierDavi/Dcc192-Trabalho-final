95ed418bbc9608f3783fe3e71a2e7bd3dd1fa984
KirchmaierDavi <davi.kirchmaier99@gmail.com>
2025-01-27 08:22:59 -0300

    Inicialização do trabalho e pagina inicial com controller iniciada

---
+++ trabalho/src/main/java/com/dcc192/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.dcc192.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("Home.jsp")
+    public String homeView() {
+        return "Home.jsp";
+    }
+}
