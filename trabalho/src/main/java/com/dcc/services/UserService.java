fb4a5b5868bdb5ad89db53503a490e3ea61eecd5
KirchmaierDavi <davi.kirchmaier99@gmail.com>
2025-02-13 12:11:05 -0300

    Mudança na user service

--- trabalho/src/main/java/com/dcc/services/UserService.java
+++ trabalho/src/main/java/com/dcc/services/UserService.java
@@ -1,8 +1,10 @@
 package com.dcc.services;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -14,19 +16,17 @@ import com.dcc.repositories.UserRepository;
 
 @Service
 public class UserService implements UserDetailsService {
-    
+
     @Autowired
     private UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username)
-            .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
-            
-        return org.springframework.security.core.userdetails.User
-            .withUsername(user.getUsername())
-            .password(user.getPassword())
-            .roles("USER")
-            .build();
+        return userRepository.findByUsername(username)
+                .map(user -> new org.springframework.security.core.userdetails.User(
+                        user.getUsername(),
+                        user.getPassword(),
+                        Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"))))
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + username));
     }
 }
